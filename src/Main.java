dc41f252cfe24d736a07ebc6944d7bd8e146557e
Perkles <isabela.rodriguescousseau@gmail.com>
2023-01-29 19:07:08 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,64 @@
+import br.com.dio.desafio.dominio.*;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descricao curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descricao curso js");
+        curso2.setCargaHoraria(4);
+
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descricao mentoria java");
+        mentoria.setData(LocalDate.now());
+
+
+        /*System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devNara = new Dev();
+        devNara.setNome("Nara");
+        devNara.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Nara" + devNara.getConteudosInscritos());
+        devNara.progredir();
+        devNara.progredir();
+        devNara.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Nara" + devNara.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Nara" + devNara.getConteudosConcluidos());
+        System.out.println("XP: " + devNara.calcularTotalXp());
+        System.out.println("-------------------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Joao" + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Joao" + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Joao" + devJoao.getConteudosConcluidos());
+        System.out.println("XP: " + devJoao.calcularTotalXp());
+
+
+
+
+
     }
 }
